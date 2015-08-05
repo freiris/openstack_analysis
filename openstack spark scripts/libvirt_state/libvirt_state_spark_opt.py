fd306249923fd74afb489265ceb7b01e723f330e
freiris <yongxiang@yongxiangs-MacBook-Pro.local>
2015-08-05 16:25:42 +0800

    indent with tab and print(sys.argv)

--- openstack spark scripts/libvirt_state/libvirt_state_spark_opt.py
+++ openstack spark scripts/libvirt_state/libvirt_state_spark_opt.py
@@ -26,44 +26,45 @@ def unique(VMStates):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: libvirt_state_spark_opt.py <pathIn> <pathOut>")
-        exit(-1)
+    	print(sys.argv)	
+    	if len(sys.argv) != 3:
+        	print("Usage: libvirt_state_spark_opt.py <pathIn> <pathOut>")
+        	exit(-1)
 
-    conf = SparkConf()
-    conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-    conf.set("spark.speculation", "true")
-    conf.setAppName("PythonLibvirtState")
-    sc = SparkContext(conf=conf)
+    	conf = SparkConf()
+    	conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    	conf.set("spark.speculation", "true")
+    	conf.setAppName("PythonLibvirtState")
+    	sc = SparkContext(conf=conf)
 
-    pathIn = sys.argv[1]
-    pathOut = sys.argv[2] 
+    	pathIn = sys.argv[1]
+    	pathOut = sys.argv[2] 
 
-    print('pathIn = %s, pathOut = %s' % (pathIn, pathOut))
+    	print('pathIn = %s, pathOut = %s' % (pathIn, pathOut))
 
-    libvirtStates = sc.textFile(pathIn)
-    libvirtParsedRDD = (libvirtStates
-			.map(lambda line: json.loads(line.strip(',\n'))))
-#            		.cache())
+    	libvirtStates = sc.textFile(pathIn)
+    	libvirtParsedRDD = (libvirtStates
+				.map(lambda line: json.loads(line.strip(',\n'))))
+#       	     		.cache())
 
 # reduceByKey leads to many retrials, and finally aborted. why?
-#    libvirtReduceRDD = (libvirtParsedRDD
-#            		.map(lambda vmState: (vmState["uuid"], [vmState]))
-#            		.reduceByKey(lambda p, q: p + q))
+#    	libvirtReduceRDD = (libvirtParsedRDD
+#       	     		.map(lambda vmState: (vmState["uuid"], [vmState]))
+#       	     		.reduceByKey(lambda p, q: p + q))
 
-    libvirtReduceRDD = (libvirtParsedRDD
-#			.filter(lambda vmState: vmState['timestamp'] >= referTimestamp) # ad-hoc
-			.map(lambda vmState: (vmState["uuid"], vmState))
-			.groupByKey())
+    	libvirtReduceRDD = (libvirtParsedRDD
+#				.filter(lambda vmState: vmState['timestamp'] >= referTimestamp) # ad-hoc
+				.map(lambda vmState: (vmState["uuid"], vmState))
+				.groupByKey())
 
-    libvirtReduceSortedRDD = (libvirtReduceRDD
-			      .mapValues(lambda vms: sorted(vms, key = (lambda vm: vm["timestamp"]), reverse = False)))
+    	libvirtReduceSortedRDD = (libvirtReduceRDD
+				      .mapValues(lambda vms: sorted(vms, key = (lambda vm: vm["timestamp"]), reverse = False)))
 
 
-    libvirtReduceSortedDiffRDD = (libvirtReduceSortedRDD
-				  .mapValues(lambda vms: unique(vms)))
+    	libvirtReduceSortedDiffRDD = (libvirtReduceSortedRDD
+					  .mapValues(lambda vms: unique(vms)))
 
-#    libvirtReduceSortedDiffRDD.map(lambda x: json.dumps(x)).saveAsTextFile(pathOut) # dump to json format, then save as textfile
-    libvirtReduceSortedDiffRDD.saveAsTextFile(pathOut) 
+#    	libvirtReduceSortedDiffRDD.map(lambda x: json.dumps(x)).saveAsTextFile(pathOut) # dump to json format, then save as textfile
+    	libvirtReduceSortedDiffRDD.saveAsTextFile(pathOut) 
 
-    sc.stop()
+    	sc.stop()
